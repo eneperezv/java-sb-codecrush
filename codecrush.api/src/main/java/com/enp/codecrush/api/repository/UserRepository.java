8b69f79ebd1ee829d5a652bf1322026f301fb283
eneperezv <eneperezv@gmail.com>
2024-12-07 23:45:59 -0500

    Implementacion de clases para autenticacion. Inclusion de dependencias.
     Changes to be committed:
    	modified:   codecrush.api/pom.xml
    	modified:   codecrush.api/src/main/java/com/enp/codecrush/api/controller/AuthController.java
    	modified:   codecrush.api/src/main/java/com/enp/codecrush/api/dto/UserDto.java
    	modified:   codecrush.api/src/main/java/com/enp/codecrush/api/model/ErrorDetails.java
    	modified:   codecrush.api/src/main/java/com/enp/codecrush/api/model/ResponseDetails.java
    	modified:   codecrush.api/src/main/java/com/enp/codecrush/api/model/Token.java
    	modified:   codecrush.api/src/main/java/com/enp/codecrush/api/model/User.java
    	modified:   codecrush.api/src/main/java/com/enp/codecrush/api/repository/UserRepository.java
    	modified:   codecrush.api/src/main/java/com/enp/codecrush/api/service/UserService.java

--- codecrush.api/src/main/java/com/enp/codecrush/api/repository/UserRepository.java
+++ codecrush.api/src/main/java/com/enp/codecrush/api/repository/UserRepository.java
@@ -1,5 +1,15 @@
 package com.enp.codecrush.api.repository;
 
-public interface UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.enp.codecrush.api.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+	
+	Optional<User> findByUsername(String username);
 
 }
